6a3d003f334bfdd66a7b152d56b716cfd73879b8
phillipbrock <89425833+phillipbrock@users.noreply.github.com>
2021-09-18 21:44:19 -0400

    Add files via upload

---
+++ Election_Analysis/pypoll.py
@@ -0,0 +1,10 @@
+voting_data = []
+counties_dict = []
+
+voting_data.append({"county":"Arapahoe", "registered_voters": 422829})
+voting_data.append({"county":"Denver", "registered_voters": 463353})
+voting_data.append({"county":"Jefferson", "registered_voters": 432438})
+
+
+for county in counties_dict:
+    print(county)
